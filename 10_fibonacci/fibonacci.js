49ceaa15464a9bd3bb56ea29ac8baf49501d1b93
Exequiel Farías <exequiel-farias@outlook.com>
2025-02-24 20:51:00 -0300

    Solucion ejercicio10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+  let num1 = 1;
+  let num2 = 1;
 
+  if (pos < 0){
+    return "OOPS";
+  }if(pos instanceof String){
+    pos = parseInt(pos);
+  }
+  if(pos == 0){
+    return 0;
+  }
+  if (pos == 1 || pos == 2){
+    return 1;
+  }else {
+    for (let i = 3; i <= pos; i++){
+      let temp = num1 + num2;
+      num1 = num2;
+      num2 = temp;
+    }
+    return num2;
+  }
 };
 
 // Do not edit below this line
