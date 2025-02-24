97db55863a3f239728f1f06e830e35041fe25f01
Exequiel Farías <exequiel-farias@outlook.com>
2025-02-24 17:57:08 -0300

    Solucion ejercicio08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  array.forEach(element => {
+    result += element;
+  });
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for(let i=0; i<array.length; i++){
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0){
+    return 1;
+  }else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
