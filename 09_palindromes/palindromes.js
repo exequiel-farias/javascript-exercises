05e97c64fd5dcf517f4fbbf904859c711e264a69
Exequiel Farías <exequiel-farias@outlook.com>
2025-02-24 19:04:28 -0300

    Solucion ejercicio09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const alfanumerico = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const stringLimpio = str
+    .toLowerCase()
+    .split('')
+    .filter(item => alfanumerico.includes(item))
+    .join('');
+
+  const stringReverso = stringLimpio.split('').reverse().join('');
+
+  return stringLimpio === stringReverso;
 };
 
 // Do not edit below this line
